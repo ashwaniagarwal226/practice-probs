2ba268628a110bfb1a6111dba0c14a386a664678
Ashwani <ashwaniagarwal226@gmail.com>
2020-07-12 10:31:21 +0530

    corrected Grades system where if grade next multiple 0f 5 is < 3 then that number

--- src/Grades.java
+++ src/Grades.java
@@ -6,10 +6,29 @@ public class Grades {
 
     public static void main(String... args) {
         List<Integer> grades = new ArrayList<>();
+        grades.add(80);
+        grades.add(96);
+        grades.add(18);
         grades.add(73);
-        grades.add(67);
-        grades.add(38);
+        grades.add(78);
+        grades.add(60);
+        grades.add(60);
+        grades.add(15);
+        grades.add(45);
+        grades.add(15);
+        grades.add(10);
+        grades.add(5);
+        grades.add(46);
+        grades.add(87);
         grades.add(33);
+        grades.add(60);
+        grades.add(14);
+        grades.add(71);
+        grades.add(65);
+        grades.add(2);
+        grades.add(5);
+        grades.add(97);
+        grades.add(0);
         gradingStudents(grades).stream().forEach(System.out::println);
     }
 
@@ -17,12 +36,13 @@ public class Grades {
         List<Integer> finalGrades = new ArrayList<>();
 
         grades.stream().forEach(grade -> {
-            if (grade >= 38) {
+             if (grade >= 38 && (grade%5) != 0) {
                 int nextNum = ((grade / 5) + 1) * 5;
                 if (nextNum - grade < 3) {
                     finalGrades.add(nextNum);
-                }
-                if (nextNum - grade == 3) {
+                }else if(nextNum-grade == 3){
+                    finalGrades.add(grade);
+                }else {
                     finalGrades.add(grade);
                 }
             } else {
@@ -32,12 +52,14 @@ public class Grades {
         });
 
         // OLD School
-        /*for (Integer grade : grades) {
-            if (grade >= 38) {
+      /*  for (Integer grade : grades) {
+            if (grade >= 38 && (grade%5) != 0) {
                 int nextNum = ((grade / 5) + 1) * 5;
                 if (nextNum - grade < 3) {
                     finalGrades.add(nextNum);
-                }if(nextNum-grade == 3){
+                }else if(nextNum-grade == 3){
+                    finalGrades.add(grade);
+                }else {
                     finalGrades.add(grade);
                 }
             } else {
