97a0fb1fca03bfc1c41c02c167ab6e090484f5c5
Ashwani <ashwaniagarwal226@gmail.com>
2020-07-13 12:16:34 +0530

    Merge Point of two linkedList

---
+++ src/MergePointTwoLinkedList.java
@@ -0,0 +1,32 @@
+public class MergePointTwoLinkedList {
+    /*URL*/
+    /*https://www.hackerrank.com/challenges/find-the-merge-point-of-two-joined-linked-lists/problem?h_r=next-challenge&h_v=zen*/
+
+
+    /*Given pointers to the head nodes of linked lists that merge together at some point, find the Node where
+    the two lists merge. It is guaranteed that the two head Nodes will be different, and neither will be NULL.
+    In the diagram below, the two lists converge at Node x :
+      [List #1] a--->b--->c
+                    \
+                     x--->y--->z--->NULL
+                    /
+         [List #2] p--->q
+
+    Complete the int FindMergeNode(Node* headA, Node* headB) method so that it finds and returns the
+    data value of the Node where the two lists merge.
+    Input Format
+    Do not read any input from stdin/console.
+            The FindMergeNode(Node*,Node*) method has two parameters, and , which are the nonnull head Nodes of two separate linked lists that are guaranteed to converge.
+
+*/
+    public static void main(String... args){
+
+
+
+    }
+
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+
+    return 0;
+    }
+}
