b03e6dd04deaebb7e6df1344333c3b844d41bd60
Ashwani <ashwaniagarwal226@gmail.com>
2020-07-19 14:57:03 +0530

    wissenTech better soln

--- src/Wissen.java
+++ src/Wissen.java
@@ -1,43 +1,45 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Wissen {
 
-   static Map<String,String> data = new HashMap<String,String>();
+    static Map<String, List<Integer>> data = new HashMap<>();
 
     public static void main(String[] args) {
 
-        putNewInnings("Virat",100);
-        putNewInnings("Virat",70);
-        putNewInnings("robin",12);
-        putNewInnings("Virat",20);
+        putNewInnings("Virat", 100);
+        putNewInnings("Virat", 70);
+        putNewInnings("robin", 12);
+        putNewInnings("robin", 12);
+        putNewInnings("Virat", 20);
+        putNewInnings("robin", 12);
+        putNewInnings("robin", 100);
+        putNewInnings("robin", 400);
 
         System.out.println(data);
         System.out.println(getAverageRuns("Virat"));
 
     }
 
-    public static void putNewInnings(String player, int runs){
-        String run = data.get(player);
-                if(run == null){
-                    run = Integer.toString(runs)+",";
-                    data.put(player,run);
-                }else{
-                    run = run + Integer.toString(runs)+",";
-                    data.put(player,run);
-                }
-    }
-    static public double getAverageRuns(String player){
-        // YOUR CODE HERE
-        String runs = data.get(player);
-        runs=runs.substring(0,runs.length()-1);
-        String[] dd = runs.split(",");
-        int sum = 0;
-        for(String ss : dd){
-            sum=sum + Integer.valueOf(ss);
+    public static void putNewInnings(String player, int runs) {
+        List<Integer> run = data.get(player);
+        if (run == null || run.size() == 0) {
+            run = new ArrayList<>();
         }
-        double avg = Double.valueOf(sum)/Double.valueOf(dd.length);
-        System.out.println(avg);
+        run.add(runs);
+        data.put(player, run);
+    }
+
+    static public double getAverageRuns(String player) {
+        // YOUR CODE HERE
+        List<Integer> runs = data.get(player);
+        int sum = 0;
+        for (Integer ss : runs) {
+            sum = sum + ss;
+        }
+        double avg = Double.valueOf(sum) / Double.valueOf(runs.size());
         return avg;
     }
 }
