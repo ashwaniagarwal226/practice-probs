9692b1aaf9b2e7a0004cd7fe885108fc40d74492
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-01 20:15:11 +0530

    intersection of two String Here tech

--- src/ArrayDS.java
+++ src/ArrayDS.java
@@ -1,26 +1,23 @@
-import java.util.Arrays;
-import java.util.Set;
-
 public class ArrayDS {
 
     public static void main(String... args) {
 
         System.out.println("ashwani");
 
-        System.out.println(getCommonString("failpass","sailpass"));
+        System.out.println(getCommonString("failpass", "sailpass"));
 
 
     }
 
 
     private static String getCommonString(String strOne, String strTwo) {
-        String finalString="";
+        String finalString = "";
 
-        for(int i=0;i<=strOne.length()-1;i++){
+        for (int i = 0; i <= strOne.length() - 1; i++) {
 
-            if(strTwo.indexOf(strOne.charAt(i)) !=-1){
+            if (strTwo.indexOf(strOne.charAt(i)) != -1) {
 
-                if(finalString.indexOf(strOne.charAt(i))==-1){
+                if (finalString.indexOf(strOne.charAt(i)) == -1) {
                     finalString = finalString + strOne.charAt(i);
                 }
 
