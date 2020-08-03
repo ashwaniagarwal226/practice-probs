2ac46c8b4c0a41faee0234a9df7be1eb820e7e9f
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-03 23:25:48 +0530

    image resize program

--- src/ImageResize.java
+++ src/ImageResize.java
@@ -6,36 +6,50 @@ import java.io.IOException;
 
 public class ImageResize {
 
-    private static final int IMG_WIDTH = 100;
-    private static final int IMG_HEIGHT = 100;
+   /* private static final int IMG_WIDTH = 413;
+    private static final int IMG_HEIGHT = 531;*/
+
+    private static final int IMG_WIDTH = 350;
+    private static final int IMG_HEIGHT = 350;
 
     public static void main(String [] args){
 
         try{
 
-            BufferedImage originalImage = ImageIO.read(new File("c:\\image\\mkyong.jpg"));
+            BufferedImage originalImage = ImageIO.read(new File("D:\\Ashwani\\Documents\\moluDocs\\photo.jpg"));
             int type = originalImage.getType() == 0? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
 
-            BufferedImage resizeImageJpg = resizeImage(originalImage, type);
-            ImageIO.write(resizeImageJpg, "jpg", new File("c:\\image\\mkyong_jpg.jpg"));
+            BufferedImage tThumbImage = new BufferedImage( IMG_WIDTH, IMG_HEIGHT, BufferedImage.TYPE_INT_RGB );
+            Graphics2D tGraphics2D = tThumbImage.createGraphics(); //create a graphics object to paint to
+            tGraphics2D.setBackground( Color.WHITE );
+            tGraphics2D.setPaint( Color.WHITE );
+            tGraphics2D.fillRect( 0, 0, IMG_WIDTH, IMG_HEIGHT );
+            tGraphics2D.setRenderingHint( RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR );
+            tGraphics2D.drawImage( originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null ); //draw the image scaled
 
-            BufferedImage resizeImagePng = resizeImage(originalImage, type);
+            ImageIO.write( tThumbImage, "jpg", new File("D:\\Ashwani\\Documents\\moluDocs\\photo_java2.jpg" ));
+
+            BufferedImage resizeImageJpg = resizeImage(originalImage, type);
+            ImageIO.write(resizeImageJpg, "jpg", new File("D:\\Ashwani\\Documents\\moluDocs\\photo_java.jpg"));
+
+            /*BufferedImage resizeImagePng = resizeImage(originalImage, type);
             ImageIO.write(resizeImagePng, "png", new File("c:\\image\\mkyong_png.jpg"));
 
             BufferedImage resizeImageHintJpg = resizeImageWithHint(originalImage, type);
             ImageIO.write(resizeImageHintJpg, "jpg", new File("c:\\image\\mkyong_hint_jpg.jpg"));
 
             BufferedImage resizeImageHintPng = resizeImageWithHint(originalImage, type);
-            ImageIO.write(resizeImageHintPng, "png", new File("c:\\image\\mkyong_hint_png.jpg"));
+            ImageIO.write(resizeImageHintPng, "png", new File("c:\\image\\mkyong_hint_png.jpg"));*/
 
 
-            File input = new File("/tmp/duke.png");
+            /*File input = new File("D:\\Ashwani\\Documents\\moluDocs\\photo.jpg");
             BufferedImage image = ImageIO.read(input);
 
             BufferedImage resized = resize(image, 500, 500);
 
-            File output = new File("/tmp/duke-resized-500x500.png");
-            ImageIO.write(resized, "png", output);
+            File output = new File("D:\\Ashwani\\Documents\\moluDocs\\photo_java.jpg");
+            ImageIO.write(resized, "jpg", output);*/
+            System.out.println("Image resize done in java");
 
         }catch(IOException e){
             System.out.println(e.getMessage());
