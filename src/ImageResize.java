ba5fac0331f302d4272376842d0f0cec33351ac6
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-03 23:00:34 +0530

    image resize program

---
+++ src/ImageResize.java
@@ -0,0 +1,81 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageResize {
+
+    private static final int IMG_WIDTH = 100;
+    private static final int IMG_HEIGHT = 100;
+
+    public static void main(String [] args){
+
+        try{
+
+            BufferedImage originalImage = ImageIO.read(new File("c:\\image\\mkyong.jpg"));
+            int type = originalImage.getType() == 0? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+
+            BufferedImage resizeImageJpg = resizeImage(originalImage, type);
+            ImageIO.write(resizeImageJpg, "jpg", new File("c:\\image\\mkyong_jpg.jpg"));
+
+            BufferedImage resizeImagePng = resizeImage(originalImage, type);
+            ImageIO.write(resizeImagePng, "png", new File("c:\\image\\mkyong_png.jpg"));
+
+            BufferedImage resizeImageHintJpg = resizeImageWithHint(originalImage, type);
+            ImageIO.write(resizeImageHintJpg, "jpg", new File("c:\\image\\mkyong_hint_jpg.jpg"));
+
+            BufferedImage resizeImageHintPng = resizeImageWithHint(originalImage, type);
+            ImageIO.write(resizeImageHintPng, "png", new File("c:\\image\\mkyong_hint_png.jpg"));
+
+
+            File input = new File("/tmp/duke.png");
+            BufferedImage image = ImageIO.read(input);
+
+            BufferedImage resized = resize(image, 500, 500);
+
+            File output = new File("/tmp/duke-resized-500x500.png");
+            ImageIO.write(resized, "png", output);
+
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private static BufferedImage resizeImage(BufferedImage originalImage, int type){
+        BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
+        g.dispose();
+
+        return resizedImage;
+    }
+
+    private static BufferedImage resize(BufferedImage img, int height, int width) {
+        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resized.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+        return resized;
+    }
+
+    private static BufferedImage resizeImageWithHint(BufferedImage originalImage, int type){
+
+        BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
+        g.dispose();
+        g.setComposite(AlphaComposite.Src);
+
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        return resizedImage;
+    }
+}
