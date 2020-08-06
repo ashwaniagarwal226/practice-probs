39f19a83545362be3598a8fb4000bb38baaee14d
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-06 17:26:18 +0530

    intersection

---
+++ src/MoRStan.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class MoRStan {
+    //
+    public static void main(String[] args) {
+        int[] a = {1, 2, 4, 9, 1,1};
+        int b[] = {4, 6, 7, 2, 1, 10, 1};
+        //1,1,2,4
+
+        getCommon(a, b).forEach(System.out::println);
+    }
+
+
+    static List getCommon(int[] arr1, int[] arr2) {
+        List<Integer> resuIntegers = new ArrayList<>();
+        for (int i = 0; i < arr1.length; i++) {
+
+            for (int j = 0; j < arr2.length; j++) {
+                if ( arr2[j]==arr1[i]) {
+                    resuIntegers.add(arr1[i]);
+                    break;
+                }
+
+            }
+        }
+        return resuIntegers;
+    }
+
+}
