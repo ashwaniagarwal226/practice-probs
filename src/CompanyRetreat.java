f1275039c6fff3d79ed0aecdd53b5e80ddb5355d
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-08 19:06:07 +0530

    Company retreat

---
+++ src/CompanyRetreat.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+
+public class CompanyRetreat {
+
+    /*https://www.hackerrank.com/challenges/company-retreat/problem*/
+
+            /*10 10
+            8 10 6 2 8 1 9 1 1
+            66802910 6
+            774086042 4
+            378549635 1
+            294879946 8
+            967907212 9
+            233866333 2
+            693327880 7
+            275512956 3
+            440500225 357103675
+            46000933 5*/
+
+            //466413254 answer
+
+            /*7 3
+            1 1 3 4 2 4
+            5 3
+            6 2
+            1 1*/
+
+            // 46 Answer
+    public static void main(String[] args) {
+
+    }
+
+    // Complete the solve function below.
+    static int solve(int[] supervisors, int[][] groups) {
+
+        return 0;
+    }
+
+
+
+}
