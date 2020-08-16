add55dd09d6c6eaacbd8140a7a6ac3e85f958502
Ashwani <ashwaniagarwal226@gmail.com>
2020-08-16 11:11:12 +0530

    array to char a b c

---
+++ src/WiproTest2.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WiproTest2 {
+
+    public static void main(String[] args) {
+        int array[] = {10, 5, 70, 1};
+        int newArr[] = array.clone();
+        char charAr[] = new char[array.length];
+
+        Arrays.sort(newArr);
+
+        Map<Integer, Character> hh = new HashMap();
+        char t ='a';
+
+        for (int i = 0; i < newArr.length; i++) {
+            hh.put(newArr[i],t++);
+
+        }
+        for(int a =0 ;a<array.length;a ++){
+            System.out.println(hh.get(array[a]));
+        }
+    }
+}
